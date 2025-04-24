eeaf9fbb31888cebdea74797ab95ffb6271f23d5
Facundo Perdomo <facuescuela47@email.com>
2025-04-24 00:57:35 -0300

    se agrega url de produccion en seguridad

--- backend/Mova/src/main/java/com/movauy/mova/config/WebSocketConfig.java
+++ backend/Mova/src/main/java/com/movauy/mova/config/WebSocketConfig.java
@@ -20,19 +20,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 1) endpoint “puro” WS/SOCKETS para brokerURL="wss://.../ws"
         registry
-            .addEndpoint("/ws")
-            .setAllowedOriginPatterns(
-                "http://localhost:3000",
-                "https://7fdc-2800-a4-11bc-8800-d561-166e-d771-2a27.ngrok-free.app"
-            );
+                .addEndpoint("/ws")
+                .setAllowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://7fdc-2800-a4-11bc-8800-d561-166e-d771-2a27.ngrok-free.app"
+                );
 
         // 2) exactamente el mismo + SockJS fallback (opcional)
         registry
-            .addEndpoint("/ws")
-            .setAllowedOriginPatterns(
-                "http://localhost:3000",
-                "https://7fdc-2800-a4-11bc-8800-d561-166e-d771-2a27.ngrok-free.app"
-            )
-            .withSockJS();
+                .addEndpoint("/ws")
+                .setAllowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://movauy.top",
+                        "https://movauy.top:8443",
+                        "https://7fdc-2800-a4-11bc-8800-d561-166e-d771-2a27.ngrok-free.app"
+                )
+                .withSockJS();
     }
 }
