96a384d557a3b255bd2546f7d05bf74608bf9e53
Facundo Perdomo <facuescuela47@email.com>
2025-07-11 18:47:25 -0300

    Se commitean cambios de la notificación de mp para que cada branch y usuario vea solo él la notificacion

--- backend/Mova/src/main/java/com/movauy/mova/controller/mercadopago/MercadoPagoController.java
+++ backend/Mova/src/main/java/com/movauy/mova/controller/mercadopago/MercadoPagoController.java
@@ -52,7 +52,7 @@ public class MercadoPagoController {
     public ResponseEntity<Map<String, Object>> createPreference(
             @PathVariable Long branchId,
             @RequestBody PaymentRequest request,
-            HttpServletRequest httpReq
+            Authentication authentication
     ) {
         log.info("🔔 createPreference invoked for branchId={} amount={}", branchId, request.getAmount());
 
@@ -66,9 +66,9 @@ public class MercadoPagoController {
         }
 
         try {
-            String rawToken = httpReq.getHeader("Authorization");
-            User me = authService.getUserEntityFromToken(rawToken);
+            User me = (User) authentication.getPrincipal();
             Long userId = me.getId();
+            
             // 2) Configurar SDK y crear preferencia
             MercadoPago.SDK.setAccessToken(accessToken);
             Preference pref = new Preference()
