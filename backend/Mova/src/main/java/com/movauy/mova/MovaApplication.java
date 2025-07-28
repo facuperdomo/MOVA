06c737c0a9b4f3783d700e05a948b22c67d231e0
Facundo Perdomo <facuescuela47@email.com>
2025-07-28 19:02:02 -0300

    Se commitean cambios de horarios

--- backend/Mova/src/main/java/com/movauy/mova/MovaApplication.java
+++ backend/Mova/src/main/java/com/movauy/mova/MovaApplication.java
@@ -1,11 +1,13 @@
 package com.movauy.mova;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovaApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Montevideo"));
         SpringApplication app = new SpringApplication(MovaApplication.class);
         app.setAdditionalProfiles("local");
         app.run(args);
